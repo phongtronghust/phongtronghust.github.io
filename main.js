ca704db45c84ffff2d6dfc9693e8a686e5a783f5
hoanmai06 <m.a.i.h.u.u.h.o.a.n@gmail.com>
2024-09-11 22:41:19 +0700

    Hỗ trợ chọn ngày giờ

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function custom_time_to_string(time) {
 }
 
 // Kiểm tra tuần hiện tại có nằm trong danh sách tuần theo định dạng của trường không
-// (8, '2-9, 11-18') = true, (10, '2-9, 11-18') = false
+// f(8, '2-9, 11-18') = true, f(10, '2-9, 11-18') = false
 function is_week_in_week_string(week, week_string) {
     for (let sub_week_string of week_string.split(',')) {
         const split_sub_week = sub_week_string.toString().split('-');
@@ -19,18 +19,8 @@ function is_week_in_week_string(week, week_string) {
     return false;
 }
 
-// Hàm trả về thứ, giờ và tuần hiện tại
-function getCurrentDayTimeWeek() {
-    const date = new Date();
-    return [
-        date.getDay() === 0 ? 6 : date.getDay() - 1,
-        date.getHours(),
-        date.getMinutes(),
-        Math.floor((date.valueOf() - monday_first_week_ms)/604800000) + 1];
-}
-
-function getTimeString(hour, minute) {
-    return (hour < 10 ? '0' : '') + hour + 'h' + (minute < 10 ? '0' : '') + minute;
+function hour_minute_second_to_string(hour, minute, second) {
+    return (hour < 10 ? '0' : '') + hour + ':' + (minute < 10 ? '0' : '') + minute + ':' + (second < 10 ? '0' : '') + second;
 }
 
 // Hàm trả về thông tin các lớp hiện tại và lớp tiếp theo của một phòng tại một tuần, thứ và giờ cụ thể
@@ -73,46 +63,83 @@ function filter() {
     }
 }
 
-const monday_first_week_ms = new Date("2024-09-02T00:00:00").valueOf();
-const current_time_element = document.getElementById("current_time");
-const list = document.getElementById("list");
-
-const current_time_information = getCurrentDayTimeWeek();
-current_time_element.innerHTML = `Thời gian: ${getTimeString(current_time_information[1], current_time_information[2])}, thứ ${current_time_information[0] + 2}, tuần ${current_time_information[3]}`;
-
-for (let i = 0; i < rooms.length; ++i) {
-    let time = current_time_information[1]*12 + current_time_information[2]/5;
-
-    let current_course, next_course;
-    [current_course, next_course] = get_information_of_room(i, current_time_information[0], time, current_time_information[3]);
-
-    let child = document.createElement("li");
-
-    let tag_html;
-    if (current_course == null && next_course == null) {
-        tag_html = '<span class="tag" style="background: gold;">Trống tới hết ngày</span>'
-    } else if (current_course == null) {
-        tag_html = '<span class="tag" style="background: lime;">Trống</span>'
-    } else {
-        tag_html = '<span class="tag" style="background: tomato;">Đang học</span>'
-    }
-
-    let current_course_html = "";
-    if (current_course != null) {
-        for (let course of current_course) {
-            current_course_html += `<div style="margin-top: 16px; text-align: center;">${(course[0] + ` - ${subjects[course[1]][1]}`)}</div>`
-            current_course_html += `<div style="display: flex;">${custom_time_to_string(course[2])} <span style="display: flex; flex: content; border-radius: 6px; border: 1px solid black; margin: 6px 12px 6px;"><span style="flex: ${(time-course[2])/(course[3] - course[2])}; border-radius: 6px; background: tomato;"></span></span> ${custom_time_to_string(course[3])}</div>`;
-        }
-    }
-
-    let next_course_html = "<div style=\"margin-top: 16px; text-align: right;\"><strong>Tiếp theo:</strong> Không</div>";
-    if (next_course != null) {
-        next_course_html = `<div style="margin-top: 16px; text-align: right;"><strong>Tiếp theo:</strong> ${custom_time_to_string(next_course[2])}, lớp ${next_course[0]} <br> ${subjects[next_course[1]][1]}</div>`
-    }
-
-    child.innerHTML =
-        `Phòng <strong>${rooms[i]}</strong> ${tag_html}
-         ${current_course_html}
-         ${next_course_html}`;
-    list.appendChild(child);
+function show_real_time() {
+    const date = new Date();
+    date_input.valueAsDate = date;
+    time_input.value = hour_minute_second_to_string(date.getHours(), date.getMinutes(), date.getSeconds());
 }
+
+function calculate_list() {
+    const day_of_week = current_date.getDay() === 0 ? 6 : current_date.getDay() - 1;
+    const hour = current_date.getHours();
+    const minute = current_date.getMinutes();
+    const week_number = Math.floor((current_date.valueOf() - FIRST_WEEK_START_MS)/WEEK_MS) + 1;
+
+    list.innerHTML = "";
+
+    for (let i = 0; i < rooms.length; ++i) {
+        const time = hour*12 + minute/5;
+
+        let current_course, next_course;
+        [current_course, next_course] = get_information_of_room(i, day_of_week, time, week_number);
+
+        let child = document.createElement("li");
+
+        let tag_html;
+        if (current_course == null && next_course == null) {
+            tag_html = '<span class="tag" style="background: gold;">Trống tới hết ngày</span>'
+        } else if (current_course == null) {
+            tag_html = '<span class="tag" style="background: lime;">Trống</span>'
+        } else {
+            tag_html = '<span class="tag" style="background: tomato;">Đang học</span>'
+        }
+
+        let current_course_html = "";
+        if (current_course != null) {
+            for (let course of current_course) {
+                current_course_html += `<div style="margin-top: 16px; text-align: center;">${(course[0] + ` - ${subjects[course[1]][1]}`)}</div>`
+                current_course_html += `<div style="display: flex;">${custom_time_to_string(course[2])} <span style="display: flex; flex: content; border-radius: 6px; border: 1px solid black; margin: 6px 12px 6px;"><span style="flex: ${(time-course[2])/(course[3] - course[2])}; border-radius: 6px; background: tomato;"></span></span> ${custom_time_to_string(course[3])}</div>`;
+            }
+        }
+
+        let next_course_html = "<div style=\"margin-top: 16px; text-align: right;\"><strong>Tiếp theo:</strong> Không</div>";
+        if (next_course != null) {
+            next_course_html = `<div style="margin-top: 16px; text-align: right;"><strong>Tiếp theo:</strong> ${custom_time_to_string(next_course[2])}, lớp ${next_course[0]} <br> ${subjects[next_course[1]][1]}</div>`
+        }
+
+        child.innerHTML =
+            `Phòng <strong>${rooms[i]}</strong> ${tag_html}
+             ${current_course_html}
+             ${next_course_html}`;
+
+        list.appendChild(child);
+    }
+}
+
+function switch_to_manual_mode() {
+    clearInterval(timer_id);
+    current_date = new Date(date_input.value + "T" + time_input.value);
+    calculate_list();
+}
+
+function switch_to_auto_mode() {
+    reload_button.style.display = 'none';
+    show_real_time();
+    timer_id = setInterval(show_real_time, 1000);
+    calculate_list();
+}
+
+const FIRST_WEEK_START_MS = new Date("2024-09-02T00:00:00").valueOf();
+const WEEK_MS = 604800000;
+
+const date_input = document.getElementById("date_input");
+const time_input = document.getElementById("time_input");
+const list = document.getElementById("list");
+const reload_button = document.getElementById("reload_button");
+
+let current_date = new Date();
+
+calculate_list();
+show_real_time();
+
+let timer_id = setInterval(show_real_time, 1000);
